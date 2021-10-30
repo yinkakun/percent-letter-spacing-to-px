1f85aa07ce1d99cbd008871da3eabb9be21e7473
Yinka Adedire <yinkakun@gmail.com>
2021-10-30 22:05:13 +0100

    restrict max input length

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useCopyToClipboard } from "usehooks-ts";
 
 type Inputs = {
@@ -16,6 +16,8 @@ const converLineHeightInPercentToPx = (
     return ((lineHeightInPercent / 100) * fontSizeInPx).toFixed(1);
 };
 
+const MAX_INPUT_LENGTH = 5;
+
 const Home: NextPage = () => {
     const {
         register,
@@ -23,7 +25,8 @@ const Home: NextPage = () => {
         watch,
         formState: { errors },
     } = useForm<Inputs>({
-        mode: "onChange",
+        mode: "all",
+        shouldUseNativeValidation: false,
     });
 
     const lineHeightInPercent = watch("lineHeightInPercent");
@@ -46,6 +49,7 @@ const Home: NextPage = () => {
             data.lineHeightInPercent,
             data.fontSizeInPx
         );
+        setLineHeightInPx(lineHeightInPx);
         copy(`${lineHeightInPx}px`);
         setIsCopied(true);
         setTimeout(() => {
@@ -64,7 +68,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="container flex items-center h-full px-6 py-8 mx-auto">
-                <div className="mx-auto border mockup-window h-full max-h-full lg:max-h-96 bg-base-200 border-base=300">
+                <div className="mx-auto border mockup-window h-full max-h-full lg:max-h-96 bg-base-200 border-base=300 w-full max-w-5xl">
                     <div className="flex flex-col items-center px-12 mt-24">
                         <h1 className="text-center mb-14 text-opacity-90">
                             Convert Line heights in Percentage value to px.
@@ -85,12 +89,23 @@ const Home: NextPage = () => {
                                         className="pr-10 input input-bordered"
                                         autoComplete="off"
                                         type="number"
+                                        maxLength={MAX_INPUT_LENGTH}
+                                        onInput={(
+                                            e: React.ChangeEvent<HTMLInputElement>
+                                        ) =>
+                                            (e.target.value =
+                                                e.target.value.slice(
+                                                    0,
+                                                    MAX_INPUT_LENGTH
+                                                ))
+                                        }
                                     />
                                     <span className="absolute top-0 right-0 flex items-center justify-center h-full px-4 border rounded-l-none rounded-r-lg bg-base-300">
                                         %
                                     </span>
                                 </label>
                             </div>
+
                             <div className="relative form-control">
                                 <label className="absolute label -top-8">
                                     <span className="text-xs label-text text-opacity-80">
@@ -103,6 +118,16 @@ const Home: NextPage = () => {
                                         className="pr-10 input input-bordered"
                                         autoComplete="off"
                                         type="number"
+                                        maxLength={MAX_INPUT_LENGTH}
+                                        onInput={(
+                                            e: React.ChangeEvent<HTMLInputElement>
+                                        ) =>
+                                            (e.target.value =
+                                                e.target.value.slice(
+                                                    0,
+                                                    MAX_INPUT_LENGTH
+                                                ))
+                                        }
                                     />
                                     <span className="absolute top-0 right-0 flex items-center justify-center h-full px-4 border rounded-l-none rounded-r-lg bg-base-300">
                                         px
@@ -110,10 +135,13 @@ const Home: NextPage = () => {
                                 </label>
                             </div>
 
-                            <div className="px-2 py-2 text-xl">=</div>
+                            <div className="px-2 py-2 text-xl ">=</div>
 
-                            <div className="flex items-center p-2 bg-white border rounded-lg">
-                                <span className="mx-4">{lineHeightInPx}px</span>
+                            <div className="flex items-center justify-between p-2 bg-white border rounded-lg">
+                                <span className="w-40 ml-4 overflow-hidden overflow-ellipsis">
+                                    <span>{lineHeightInPx}</span>
+                                    <span className="mr-4">px</span>
+                                </span>
                                 <button
                                     type="submit"
                                     className={`w-20 text-white rounded-lg btn btn-primary btn-sm transition-all ${
